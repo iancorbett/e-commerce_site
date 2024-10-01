1985b1163bd217f5d7d7fcd50f0b6972639e0b1b
iancorbett <iancorbett1324@gmail.com>
2024-09-30 23:51:44 -0600

    Add files via upload

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+// const { TABLOCK } = require('sequelize/types/lib/table-hints');
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{model: Product}],
+    });
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      req.body, 
+      {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const tagData =   await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
